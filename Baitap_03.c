744fbfa25b031d993812d8d048f66edcfb1cc9c0
hungphi96 <106594281+hungphi96@users.noreply.github.com>
2022-06-04 23:31:29 +0700

    Add files via upload

---
+++ Baitap_03.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+main(){
+float num1, num2;
+printf("Nhap so num1 = ");
+scanf("%f", &num1);
+printf("Nhap so num2 = ");
+scanf("%f", &num2);
+printf("Tong cua so do la: %f\n", num1+num2);
+printf("Hieu cua so do la: %f\n", num1-num2);
+printf("Tich cua so do la: %f\n", num1*num2);
+printf("Thuong cua so do la: %f\n", num1/num2);
+
+getch();
+}
