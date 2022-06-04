ebe0935b11e68f4c0acde02d79d293600fb73ba7
hungphi96 <106594281+hungphi96@users.noreply.github.com>
2022-06-04 23:30:37 +0700

    Add files via upload

---
+++ baitap1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[])
+{
+	printf("----------------------------------------\n");
+	printf("* Class: T2205E1                       *\n");
+	printf("* RolNO: TH2205035                     *\n");
+	printf("* Full Name: Nguyen Hung Phi           *\n");
+	printf("* Email: hungphi12121996@gmail.com     *\n");
+	printf("* Phone: 0971484701                    *\n");
+	printf("----------------------------------------\n");
+	return 0;
+}
